d2add9cedc92c17e2117663544de00903fd4ca11
itspankaj143 <itsmelovezero@gmail.com>
2023-11-19 19:49:23 +0530

    file added

---
+++ src/Board.jsx
@@ -0,0 +1,109 @@
+import Square from './Square'
+
+function Board({ xIsNext, squares, onPlay }) {
+	console.log(squares)
+	console.log(xIsNext)
+	console.log(onPlay)
+	function handleClick(i) {
+		if (calculateWinner(squares) || squares[i]) {
+			return
+		}
+		const nextSquares = squares.slice()
+		if (xIsNext) {
+			nextSquares[i] = 'X'
+		} else {
+			nextSquares[i] = 'O'
+		}
+		onPlay(nextSquares)
+	}
+
+	const winner = calculateWinner(squares)
+	let status
+	if (winner) {
+		status = 'Winner: ' + winner
+	} else {
+		status = 'Next player: ' + (xIsNext ? 'X' : 'O')
+	}
+
+	function calculateWinner(squares) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		]
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i]
+			if (
+				squares[a] &&
+				squares[a] === squares[b] &&
+				squares[a] === squares[c]
+			) {
+				return squares[a]
+			}
+		}
+		return null
+	}
+
+	return (
+		<>
+			<div className='status'>{status}</div>
+			<div className='board-row'>
+				<Square
+					className='tic-tac-too'
+					value={squares[0]}
+					onSquareClick={() => handleClick(0)}
+				/>
+				<Square
+					className='tic-tac-too'
+					value={squares[1]}
+					onSquareClick={() => handleClick(1)}
+				/>
+				<Square
+					className='tic-tac-too'
+					value={squares[2]}
+					onSquareClick={() => handleClick(2)}
+				/>
+			</div>
+			<div className='board-row'>
+				<Square
+					className='tic-tac-too'
+					value={squares[3]}
+					onSquareClick={() => handleClick(3)}
+				/>
+				<Square
+					className='tic-tac-too'
+					value={squares[4]}
+					onSquareClick={() => handleClick(4)}
+				/>
+				<Square
+					className='tic-tac-too'
+					value={squares[5]}
+					onSquareClick={() => handleClick(5)}
+				/>
+			</div>
+			<div className='board-row'>
+				<Square
+					className='tic-tac-too'
+					value={squares[6]}
+					onSquareClick={() => handleClick(6)}
+				/>
+				<Square
+					className='tic-tac-too'
+					value={squares[7]}
+					onSquareClick={() => handleClick(7)}
+				/>
+				<Square
+					className='tic-tac-too'
+					value={squares[8]}
+					onSquareClick={() => handleClick(8)}
+				/>
+			</div>
+		</>
+	)
+}
+export default Board
